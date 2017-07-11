1a53b4a0f2fe324ec61aaf6e1d3c680491fd84cc
Jessica <jdhpabst@gmail.com>
2017-07-11 15:34:39 -0600

    finished try1, all completed and correct

--- assessment.js
+++ assessment.js
@@ -4,9 +4,9 @@
    var hello = 'goodbye';
 
 // #2 Use the variable iLove to create a new variable called iLoveCode that is assigned the string "I love code"
-   var iLove = 'I love';
-
-   var iLoveCode = iLove + ' ' + 'code';
+    var iLove = 'I love';
+	var iLoveCode = iLove + ' code'
+  
 
 // #3  Make an object called bob and give it the following properties
 // bob has a height of 6ft (string)
@@ -15,46 +15,43 @@
 // bob is not presidentOfTheUnitedStates (boolean)
 // bob likes apples, bananas, and cherries (array of strings)
 
+
 var bob = {
 	height: '6ft',
 	age: 24,
 	hair: {
 		style: 'spikey',
-		color: 'brown',
+		color: 'brown'
 	},
 	presidentOfTheUnitedStates: false,
-	likes: ['apples','bananas','cherries']
+	likes: ['apples', 'bananas', 'cherries']
 }
+
 // #4 Change my shirt color to pink using dot notation
 var myShirt = {
 	type: 'polo',
 	color: 'red'
 };
 
-   myShirt.color = 'pink';
+myShirt.color = 'pink'
+   
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
 	type: 'polo',
 	color: 'red'
 };
 
-   myOtherShirt['type'] = 'spandex';
+myOtherShirt['type'] = 'spandex';
+
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
 
 var zoo = {
-   monkeys: 8,
-   giraffes: 4,
-   elephants: 2,
-
+	monkeys: 8,
+	giraffes: 4,
+	elephants: 2
 }
-
-
-
-
-
-
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
@@ -66,17 +63,23 @@ var snake = {
 	runs: 'legless'
 };
 
-    for(var i in snake){
-		if(i.startsWith('s')) {
-			snake[i] = 's';
-		}
+    function makeS(obj){
+		for(var prop in snake){
+			// console.log(snake[prop])
+				if(prop.charAt(0) == 's'){
+					snake[prop] = 's'
+				}
+			}
+		return snake
 	}
+
+	makeS(snake)
 
 
 
 //#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
 
-var rainbowColors = ['red','orange','yellow','green','blue','indigo','violet'];
+var rainbowColors = ['red','orange','yellow','green','blue','indigo','violet']
 
 
 // #8 Using this array do the following
@@ -86,12 +89,11 @@ heroes.push('wonderwoman');
 // remove 'superman' and store him in a variable called firstHero
 var firstHero = heroes.shift();
 // add 'spongebob' to the start of the array
-heroes.unshift('spongebob');
+heroes.unshift('spongebob')
 // remove 'flash' from the array and store him in a variable called secondHero
-var secondHero = heroes.splice(2,1);
+var secondHero = heroes.splice(2,1)
 // leave batman in the array but put a copy of him on a variable called thirdHero
-var thirdHero = heroes[1];
-
+var thirdHero = heroes[1]
 
 
 //#9 Write a function called addItem that takes in an array and an item, adds the item to
@@ -99,79 +101,75 @@ var thirdHero = heroes[1];
 
 function addItem(arr, item){
 	arr.push(item);
+	return arr
+}
+
+//#10 Write a function called removeItem that takes in an array of strings, and a string.
+// Removes all instances of that string from the array. And return the modified array.
+// The order of the array should not be changed
+
+function removeItem(arr, str){
+	for(var i = arr.length - 1; i >= 0; i--){
+		if(arr[i] === str){
+			arr.splice(i, 1);
+		}
+	}
 	return arr;
 }
 
-
-//#10 Write a function called removeItem that takes in an array of strings, and a string.
-
-// Removes all instances of that string from the array. And return the modified array.
-// The order of the array should not be changed
-function removeItem(arr, str){
-   for(var i = arr.length - 1; i >= 0; i--){
-	   if(arr[i] === str){
-		   arr.splice(i, 1);
-	   }
-   }
-   return arr;
-}
-
-// !!!!!! #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
+// !!! #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
  function doubleTheFun(param){
-	 if(typeof (param) === "number"){
-	   console.log(param);
-	   return (+param) + (+param);
-	 } else {
-	   return param + param;
-	 }
+	// need to account for strings, numbers, and numbers in strings(they need to be treated like numbers)...
+	if(isNaN(param)){
+	  	return param + param
+	} else if(typeof(parseInt(param)) === 'number'){
+	  return parseFloat(param) + parseFloat(param); 
+	} 
  }
  
- doubleTheFun('5');
- 
 
-//!!!!! #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
+// !!! #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
 // return the value from the object that corresponds to the property
 
-
-function getValueOfProperty(obj, property){
-	return obj[property];
+function getValueOfProperty(obj, prop){
+  for(var i in obj){
+    if(i === prop){
+      return obj[i];
+    }
+  }
+	
 }
-
-
 
 //#13 Write a function called makeChatMessage that takes in a message and author as parameters
 // and returns an object with a message, author, and timestamp, that is
 // the current time as a Date object
 
-function makeChatMessage(message, author, timestamp){
-    var chat = {
-		message: message,
-		author: author,
-		timestamp: new Date(),
-	}
-
-	return chat;
+function makeChatMessage(message, author){
+  return {
+    message: message,
+    author: author,
+    timestamp: new Date()
+  };
 }
-
-
 
 // #14 Create a function called coderTest that takes in an object that is a person. It looks to see if the person’s 
 // name is Jeremy and then changes the person object to have a property called lovesCode with a value of 10.  
 // If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
 
 function coderTest(obj){
+  for(var i in obj){
     if(obj.name === 'Jeremy'){
-		obj.lovesCode = 10;
-	} else if(obj.name === 'Brack'){
-		obj.lovesCode = 0;
-	} else {
-		obj.lovesCode = 5;
-	}
+      obj.lovesCode = 10;
+    } else if(obj.name === 'Brack'){
+      obj.lovesCode = 0;
+    } else {
+      obj.lovesCode = 5;
+    }
+  }
+  
+  return obj;
 }
-
-
-
 
 // #15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
 /*
@@ -181,24 +179,29 @@ function coderTest(obj){
     temperature over 80 or humidity over 50 or cloudiness over 50 - return "Hmm, probably not"
     Otherwise - return "I love outside"
 */
-
-function outside(temp, humid, cloud){
-    if(temp > 80 && humid > 40){
-		return "I'm all sweat";
-	} else if(temp < 40 && cloud > 60){
-		return "I have icecicles";
-	} else if(temp > 80 && humid < 40 && cloud < 20){
-		return "I'm literally in the desert";
-	} else if(temp > 80 || humid > 50 || cloud > 50){
-		return "Hmm, probably not";
-	} else {
-		return "I love outside";
-	}
+function outside(temp, humid, clouds){
+  if(temp > 80 && humid > 40){
+    return "I'm all sweat";
+  } else if(temp < 40 && clouds > 60){
+    return "I have icecicles";
+  } else if(temp > 80 && humid < 40 && clouds < 20){
+    return "I'm literally in the desert";
+  } else if(temp > 80 || humid > 50 || clouds > 50){
+    return "Hmm, probably not";
+  } else {
+    return "I love outside"
+  }
 }
 
-// #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the argument string(back) + ' back'."
+// !!! #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the argument string(back) + ' back'."
 // example - If I call you with 'Give it' you should invoke holla with 'Give it back'
 
-function callerBack(cb, str){
-   cb(str + ' back');
+function callerBack(holla, back){
+  return holla(back + ' back');
 }
+
+function holla(str){
+  return str + ' back';
+}
+
+
